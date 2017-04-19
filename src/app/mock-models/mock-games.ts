5978293cc2fb64b83407097a19186f506545f2da
CapitalistHippie <simon_vd_meer@live.nl>
2017-04-19 19:11:57 +0200

    Removed bootstrap

--- src/app/mock-models/mock-games.ts
+++ src/app/mock-models/mock-games.ts
@@ -1,6 +1,6 @@
 import { Game } from '../models';
 
 export const MOCK_GAMES: Game[] = [
-    { _id: '57b9d324106c6811006e9c29', id: '57b9d324106c6811006e9c29', minPlayers: 2, maxPlayers: 4 },
-    { _id: '57b9d324106c6811006e9c28', id: '57b9d324106c6811006e9c28', minPlayers: 5, maxPlayers: 9 }
+    // { _id: '57b9d324106c6811006e9c29', id: '57b9d324106c6811006e9c29', minPlayers: 2, maxPlayers: 4 },
+    // { _id: '57b9d324106c6811006e9c28', id: '57b9d324106c6811006e9c28', minPlayers: 5, maxPlayers: 9 }
 ];
