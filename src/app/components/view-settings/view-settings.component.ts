5032f5b80088202c6512bf237094cb6bac41021c
CapitalistHippie <simon_vd_meer@live.nl>
2017-05-23 22:40:42 +0200

    Some cleanup

--- src/app/components/view-settings/view-settings.component.ts
+++ src/app/components/view-settings/view-settings.component.ts
@@ -19,7 +19,7 @@ export class ViewSettingsComponent implements OnInit {
 
   private exampleTiles: Tile[];
 
-  private themeNames: string[];
+  private tileNames: string[];
 
   constructor(private themeService: ThemeService) {
     this.selectedTheme = themeService.getActiveTheme();
@@ -27,51 +27,44 @@ export class ViewSettingsComponent implements OnInit {
 
     this.exampleTiles = [];
     let exampleTileBamboo = new Tile();
-    exampleTileBamboo._id = 0;
     exampleTileBamboo.name = '1';
     exampleTileBamboo.suit = 'bamboo';
     this.exampleTiles.push(exampleTileBamboo);
 
     let exampleTileCharacter = new Tile();
-    exampleTileCharacter._id = 1;
     exampleTileCharacter.name = '1';
     exampleTileCharacter.suit = 'character';
     this.exampleTiles.push(exampleTileCharacter);
 
     let exampleTileCircle = new Tile();
-    exampleTileCircle._id = 2;
     exampleTileCircle.name = '1';
     exampleTileCircle.suit = 'circle';
     this.exampleTiles.push(exampleTileCircle);
 
     let exampleTileWind = new Tile();
-    exampleTileWind._id = 3;
     exampleTileWind.name = 'north';
     exampleTileWind.suit = 'wind';
     this.exampleTiles.push(exampleTileWind);
 
     let exampleTileDragon = new Tile();
-    exampleTileDragon._id = 4;
     exampleTileDragon.name = 'red';
     exampleTileDragon.suit = 'dragon';
     this.exampleTiles.push(exampleTileDragon);
 
     let exampleTileFlower = new Tile();
-    exampleTileFlower._id = 5;
     exampleTileFlower.name = 'chrysanthemum';
     exampleTileFlower.suit = 'flower';
     this.exampleTiles.push(exampleTileFlower);
 
     let exampleTileSeason = new Tile();
-    exampleTileSeason._id = 6;
     exampleTileSeason.name = 'summer';
     exampleTileSeason.suit = 'season';
     this.exampleTiles.push(exampleTileSeason);
 
-    this.themeNames = [];
-    this.themeNames.push('1');
-    this.themeNames.push('2');
-    this.themeNames.push('3');
+    this.tileNames = [];
+    this.tileNames.push('1');
+    this.tileNames.push('2');
+    this.tileNames.push('3');
   }
 
   ngOnInit(): void {
@@ -79,6 +72,5 @@ export class ViewSettingsComponent implements OnInit {
 
   onThemeSelected(): void {
     this.themeService.setActiveTheme(this.selectedTheme);
-    console.log(this.exampleTiles);
   }
 }
